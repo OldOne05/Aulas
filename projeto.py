41c704619f993602be170aeb103863eb2c0b3bf6
Cauã Muliterno Marinho Campos <cauacampos05@gmail.com>
2021-06-21 16:30:05 -0300

    1.1

---
+++ projeto.py
@@ -0,0 +1,31 @@
+from random import randint
+
+win = 0
+lose = 0
+
+rodadas = int(input("Quantas rodadas você quer jogar?"))
+cpu = ["Pedra", "Papel", "Tesoura"]
+
+for i in range(rodadas):
+    player = input("Qual sua escolha? [Pedra, Papel, Tesoura]\n").replace(" ","").capitalize()
+    pc = randint(0,2)
+    print(cpu[pc])
+    if player == "Pedra" and cpu[pc] == "Tesoura":
+        win += 1
+    if player == "Pedra" and cpu[pc] == "Papel":
+        lose += 1
+    if player == "Tesoura" and cpu[pc] == "Papel":
+        win += 1
+    if player == "Tesoura" and  cpu[pc] == "Pedra":
+        lose += 1
+    if player == "Papel" and cpu[pc] == "Pedra":
+        win += 1
+    if player == "Papel" and cpu[pc] == "Tesoura":
+        lose += 1
+
+if win < lose:
+    print("Você perdeu, sou bem melhor que você!! ;)")
+if win > lose:
+    print("Você ganhou, por pura sorte!! >:(")
+if win == lose:
+    print("Empatamos, quer tentar de novo ou está com medo?")
